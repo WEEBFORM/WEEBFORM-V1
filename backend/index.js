c20c1a787078314038342486d691636d2f72ef90
Chimmezirim Ibekwe <nnamdichimmezirim@gmail.com>
2024-11-30 09:46:56 +0100

    ec2 config

--- backend/index.js
+++ backend/index.js
@@ -104,5 +104,4 @@ const server = http.createServer(app);
  
 server.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
-    // console.log(`Access the server at http://${IP}:${port}`);
 });
