be46d04e0f2b2ee68e1a070bbc8a1b18acd3ae79
Chimmezirim Ibekwe <nnamdichimmezirim@gmail.com>
2025-01-05 21:38:16 +0100

    expo upgrade

--- backend/middlewares/mailTransportConfig.js
+++ backend/middlewares/mailTransportConfig.js
@@ -7,7 +7,7 @@ export const transporter = nodemailer.createTransport({
     port: 587,
     secure: false, 
     auth: {
-        user: process.env.EMAIL_USER,
+        user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS,
     },
     tls: {
