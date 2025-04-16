e4b66b16928c4f27aeac4691648a58dbc948fa98
Chimmezirim Ibekwe <nnamdichimmezirim@gmail.com>
2025-04-16 10:44:11 +0100

    Edit user info and password, marketplace ratings, visits and analytics

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,19 @@
 import express from "express";
-import {viewUsers, viewProfile, viewUserProfile, editProfile, deleteAccount} from "../controllers/Users/user.js"
+import { viewUsers,
+         viewProfile,
+         viewUserProfile, 
+         editProfile, 
+         editPassword,
+         deleteAccount
+} from "../controllers/Users/user.js"
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', viewUsers)
-router.get('/user', viewProfile)
-router.get('/:id', viewUserProfile)
-router.put('/edit', editProfile)
-router.delete('/delete', deleteAccount)
+router.get('/', viewUsers);
+router.get('/user', viewProfile);
+router.get('/:id', viewUserProfile);
+router.put('/edit', editProfile);
+router.put('/change-password', editPassword);
+router.delete('/delete', deleteAccount);
 
 export default router
